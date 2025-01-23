eacfdd6ca2afbc02e5d5862aaaa623dc193013e0
= <=>
2025-01-23 15:25:34 +0530

    added faq sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 "use client";
 
+import FaqQuestions from "@/components/faq-questions/Faq-questions";
 import MainHero from "@/components/hero/MainHero";
+
 import TestimonialSlider from "@/components/testimonial_sider/TestiMonialSider";
 import Image from "next/image";
 import Link from "next/link";
@@ -816,73 +818,14 @@ export default function Home() {
 
 
       {/* Customer reviews goes here  */}
-      {/* <div className="bg-[#1C4670] h-[513px] p-10">
-        <div className="header flex justify-between">
-          <h1 className="font-bold text-3xl tracking-wider text-white">
-            What people say about us
-          </h1>
-          <div className="slider flex gap-4">
-            <div className="left cursor-pointer">
-              <Image src="/leftArrow.png" width={42.86} height={42.86} alt="Left arrow" />
-            </div>
-            <div className="right cursor-pointer">
-              <Image src="/rightArrow.png" width={42.86} height={42.86} alt="Right arrow" />
-            </div>
-          </div>
-        </div>
-
-        <div className="cards flex flex-wrap gap-4 mt-10">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <div key={index} className="card bg-white w-[520px] my-10 rounded-md shadow-lg">
-              <div className="ratingAndQuote flex justify-between p-5">
-                <svg
-                  width="32"
-                  height="28"
-                  viewBox="0 0 32 28"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M18.2084 28V19.5804C18.2084 14.2937 19.3502 9.98602 21.6338 6.65734C23.9775 3.2634 27.4329 1.04429 32 0V6.36364C29.9568 6.88578 28.3343 7.8648 27.1324 9.3007C25.9305 10.6713 25.1493 12.3357 24.7887 14.2937H30.1972V28H18.2084ZM0 28V19.5804C0 14.2937 1.14178 9.98602 3.42535 6.65734C5.76901 3.2634 9.19437 1.04429 13.7014 0V6.36364C11.6582 6.88578 10.0357 7.8648 8.8338 9.3007C7.63192 10.6713 6.8507 12.3357 6.49014 14.2937H11.8986V28H0Z"
-                    fill="url(#gradient-{index})"
-                  />
-                  <defs>
-                    <linearGradient
-                      id={`gradient-${index}`}
-                      x1="16"
-                      y1="0"
-                      x2="16"
-                      y2="28"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stopColor="#FFA229" />
-                      <stop offset="0.78125" stopColor="#1C4670" />
-                    </linearGradient>
-                  </defs>
-                </svg>
-
-                <Image src="/reviews/Star.png" width={139} height={29} alt="Rating stars" />
-              </div>
-
-              <p className="text-gray-600 p-5">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat aute irure sint amet occaecat cupidatat non proident ea
-                commodo consequat aute irure sint amet occaecat cupidatat non proident.
-              </p>
-
-              <div className="user-section p-5 flex items-center gap-3 text-black">
-                <Image src="/reviews/user1.png" width={70} height={70} alt="User profile" />
-                <div className="info">
-                  <h1 className="font-bold text-sm">Chris</h1>
-                  <p className="font-light text-xs">President and CEO, PrintReach, USA</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div> */}
-
       <TestimonialSlider />
+
+
+      {/* Faq Questions start here */}
+      <FaqQuestions />
+
+      {/* Manage Your Service */}
+
 
 
     </>
---
+++ src/components/faq-questions/Faq-questions.jsx
@@ -0,0 +1,171 @@
+import React from "react";
+import Image from "next/image";
+
+const FaqQuestions = () => {
+  return (
+    <div className="bg-white p-16">
+      <main className="flex flex-col items-center justify-center gap-7">
+        <header>
+          <h1 className="text-center text-[#EB8D15]">FAQ</h1>
+          <h1 className="text-2xl text-black font-bold  tracking-wider my-4">
+            Frequent Ask Questions{" "}
+          </h1>
+        </header>
+
+        <div className="card flex items-center justify-between  gap-16 text-black w-[1272px] h-[70px] border-2 p-5 relative overflow-hidden rounded-l-md">
+          <Image
+            src="/faq/letBar.png"
+            width={10}
+            height={0}
+            alt="left bar"
+            className="absolute -top-[1px] -left-[4px]"
+          />
+          <h1 className="bg-white z-30">
+            Can I recover deleted files from desktop with this software?
+          </h1>
+          <svg
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.9 8L9.90002 7L2.90002 0H1.90002L0.900024 1V3L6.90002 8L0.900024 14V15L1.90002 16H2.90002L9.90002 9L10.9 8Z"
+              fill="#555555"
+            />
+          </svg>
+        </div>
+
+        <div className="card flex items-center justify-between  gap-16 text-black w-[1272px] h-[70px] border-2 p-5 relative overflow-hidden rounded-l-md">
+          <Image
+            src="/faq/letBar.png"
+            width={10}
+            height={0}
+            alt="left bar"
+            className="absolute -top-[1px] -left-[4px]"
+          />
+          <h1 className="bg-white z-30">
+            Can I recover deleted files from desktop with this software?
+          </h1>
+          <svg
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.9 8L9.90002 7L2.90002 0H1.90002L0.900024 1V3L6.90002 8L0.900024 14V15L1.90002 16H2.90002L9.90002 9L10.9 8Z"
+              fill="#555555"
+            />
+          </svg>
+        </div>
+
+        <div className="card flex items-center justify-between  gap-16 text-black w-[1272px] h-[70px] border-2 p-5 relative overflow-hidden rounded-l-md">
+          <Image
+            src="/faq/letBar.png"
+            width={10}
+            height={0}
+            alt="left bar"
+            className="absolute -top-[1px] -left-[4px]"
+          />
+          <h1 className="bg-white z-30">
+            Can I recover deleted files from desktop with this software?
+          </h1>
+          <svg
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.9 8L9.90002 7L2.90002 0H1.90002L0.900024 1V3L6.90002 8L0.900024 14V15L1.90002 16H2.90002L9.90002 9L10.9 8Z"
+              fill="#555555"
+            />
+          </svg>
+        </div>
+        <div className="card flex items-center justify-between  gap-16 text-black w-[1272px] h-[70px] border-2 p-5 relative overflow-hidden rounded-l-md">
+          <Image
+            src="/faq/letBar.png"
+            width={10}
+            height={0}
+            alt="left bar"
+            className="absolute -top-[1px] -left-[4px]"
+          />
+          <h1 className="bg-white z-30">
+            Can I recover deleted files from desktop with this software?
+          </h1>
+          <svg
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.9 8L9.90002 7L2.90002 0H1.90002L0.900024 1V3L6.90002 8L0.900024 14V15L1.90002 16H2.90002L9.90002 9L10.9 8Z"
+              fill="#555555"
+            />
+          </svg>
+        </div>
+
+        <div className="card flex items-center justify-between  gap-16 text-black w-[1272px] h-[70px] border-2 p-5 relative overflow-hidden rounded-l-md">
+          <Image
+            src="/faq/letBar.png"
+            width={10}
+            height={0}
+            alt="left bar"
+            className="absolute -top-[1px] -left-[4px]"
+          />
+          <h1 className="bg-white z-30">
+            Can I recover deleted files from desktop with this software?
+          </h1>
+          <svg
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.9 8L9.90002 7L2.90002 0H1.90002L0.900024 1V3L6.90002 8L0.900024 14V15L1.90002 16H2.90002L9.90002 9L10.9 8Z"
+              fill="#555555"
+            />
+          </svg>
+        </div>
+
+        <button className="bg-[#1C4670] p-2 flex justify-center items-center gap-3">
+          Show more{" "}
+          <svg
+            width="14"
+            height="12"
+            viewBox="0 0 14 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.66667 6H12.5M8.33333 1L12.7441 5.41074C13.0695 5.73618 13.0695 6.26382 12.7441 6.58926L8.33333 11"
+              stroke="white"
+              stroke-width="1.67"
+              stroke-linecap="round"
+            />
+          </svg>
+        </button>
+      </main>
+    </div>
+  );
+};
+
+export default FaqQuestions;
