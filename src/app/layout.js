5c6223bff6f644574118205053ab183b8b4765f0
= <=>
2025-02-09 16:58:13 +0530

    added changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         <ContactHeader />
         <MainHeader />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
